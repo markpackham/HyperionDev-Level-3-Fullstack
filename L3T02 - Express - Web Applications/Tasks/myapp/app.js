99ce4af550a161b52dec576ab88298e267c19069
mark <markpackham1@gmail.com>
2023-10-26 07:13:24 +0100

    demo use of public file

--- L3T02 - Express - Web Applications/Tasks/myapp/app.js
+++ L3T02 - Express - Web Applications/Tasks/myapp/app.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
+// Use the public folder for stuff we want users to see like "hello.html"
+app.use(express.static("public"));
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
