14997cdf97a2f78be29859470059fcd6010cf8b0
mark <markpackham1@gmail.com>
2023-10-26 08:49:04 +0100

    add directions to contact us

--- L3T02 - Express - Web Applications/Compulsory Task 1/app.js
+++ L3T02 - Express - Web Applications/Compulsory Task 1/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 // Either use the environment variable for Port or Port 3000 if there isn't one
@@ -12,6 +13,15 @@ app.listen(3000, () => {
 
 app.use(express.static("public"));
 
+// Learned to use sendFile to get Html files from
+//262588213843476 (no date) ! .gitignore for Express App, Gist. Available at: https://gist.github.com/dphurley/182ddab5a2482fbdda2de3b09bff446a (Accessed: 26 October 2023).
+//Sev, C. (2021) How to deliver HTML files with Express, DigitalOcean.
+// Available at: https://www.digitalocean.com/community/tutorials/use-expressjs-to-deliver-html-files (Accessed: 26 October 2023).
+
 app.get("/about", function (req, res) {
-  res.send("Hello World!");
+  res.sendFile(path.join(__dirname, "/public/about.html"));
+});
+
+app.get("/contact_us", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/contact_us.html"));
 });
