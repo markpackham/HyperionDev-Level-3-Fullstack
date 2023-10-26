c481b6348ca88f663eec44bfa5f0a3976ee31881
mark <markpackham1@gmail.com>
2023-10-26 08:31:50 +0100

    use variable for port

--- L3T02 - Express - Web Applications/Compulsory Task 1/app.js
+++ L3T02 - Express - Web Applications/Compulsory Task 1/app.js
@@ -3,11 +3,11 @@ const app = express();
 
 // Either use the environment variable for Port or Port 3000 if there isn't one
 // http://localhost:3000/about
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // Make sure I am using Port 3000
 app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.use(express.static("public"));
