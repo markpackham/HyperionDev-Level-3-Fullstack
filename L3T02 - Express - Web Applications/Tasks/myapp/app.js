8109452a87b324806c82ed58aecc4f54031c510f
mark <markpackham1@gmail.com>
2023-10-26 07:19:37 +0100

    remove unneeded port listen

--- L3T02 - Express - Web Applications/Tasks/myapp/app.js
+++ L3T02 - Express - Web Applications/Tasks/myapp/app.js
@@ -6,9 +6,6 @@ app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
-app.listen(8000, function () {
-  console.log("Example app listening on port 8000!");
-});
 
 // Show environment variables if we have any
 // OBVIOUSLY never do this on a real site for security reasons
