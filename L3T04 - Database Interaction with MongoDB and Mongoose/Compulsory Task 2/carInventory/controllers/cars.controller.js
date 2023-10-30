d6f97845b7f530f6af40a7089f00c72d4dd77600
mark <markpackham1@gmail.com>
2023-10-30 16:21:01 +0000

    tidy up Car model structure

--- L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/controllers/cars.controller.js
+++ L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/controllers/cars.controller.js
@@ -1,106 +1,106 @@
-const Car = require('../models/car.model');
+const Car = require("../models/car.model");
 
 exports.create = async (req, res) => {
-    try {
-        // Create a new car
-        const carModel = new Car({
-            Model: 2020,
-            Make: 'Flying Car',
-            Owner: 'Neil Jameson',
-            Registration: 'ZZZ111',
-            Address: '11 Park Lane, London England',
-        });
+  try {
+    // Create a new car, use params submitted from user's POST
+    const carModel = new Car({
+      Model: req.body.Model,
+      Make: req.body.Make,
+      Owner: req.body.Owner,
+      Registration: req.body.Registration,
+      Address: req.body.Address,
+    });
 
-        // Save the new car
+    // Save the new car
     const saveCar = await carModel.save();
 
     // Success response
     console.log(saveCar);
-    res.send('The car has been added');
-} catch (error) {
+    res.send("The car has been added");
+  } catch (error) {
     // Error response
     console.error(error);
     res.status(500).send({
-        message: "Some error occurred while creating the car."
+      message: "Some error occurred while creating the car.",
     });
-    }
+  }
 };
 
 exports.findAll = (req, res) => {
-    // Use the "find" method to return all cars
-    console.log(req.params);
-    Car.find()
-        .then(cars => {
-            // Send the retrieved cars as a success response
-            res.send(cars);
-        })
-        .catch(err => {
-            // Error response
-            console.log(err);
-            res.status(500).send({
-                message: "An error occurred while retrieving cars"
-            });
-        });
+  // Use the "find" method to return all cars
+  console.log(req.params);
+  Car.find()
+    .then((cars) => {
+      // Send the retrieved cars as a success response
+      res.send(cars);
+    })
+    .catch((err) => {
+      // Error response
+      console.log(err);
+      res.status(500).send({
+        message: "An error occurred while retrieving cars",
+      });
+    });
 };
 
 exports.findOlderThan5Years = (req, res) => {
-    // Use the "find" method to return all cars
-    Car.find({ Model: { $lt: 2018 } })
-        .then(cars => {
-            // Send the retrieved cars as a success response
-            res.send(cars);
-        })
-        .catch(err => {
-            // Error response
-            console.log(err);
-            res.status(500).send({
-                message: "An error occurred while retrieving cars"
-            });
-        });
+  // Use the "find" method to return all cars
+  Car.find({ Model: { $lt: 2018 } })
+    .then((cars) => {
+      // Send the retrieved cars as a success response
+      res.send(cars);
+    })
+    .catch((err) => {
+      // Error response
+      console.log(err);
+      res.status(500).send({
+        message: "An error occurred while retrieving cars",
+      });
+    });
 };
 
-
-
 exports.updateByReg = async (req, res) => {
-    try {
-        // Grab specific car to update by reg
-        const reg = req.params.reg;
+  try {
+    // Grab specific car to update by reg
+    const reg = req.params.reg;
 
-        // Define the new data to update the owner
-        const update = { Owner: 'Richard Liverpool' };
+    // Define the new data to update the owner
+    const update = { Owner: "Richard Liverpool" };
 
-        /* Use the "findOneAndUpdate" method to update a car with the
+    /* Use the "findOneAndUpdate" method to update a car with the
         specified reg and set the "new" option to true to get the
         updated document as the result */
-        // The find part requires an object rather than a string
-        const updatedCar = await Car.findOneAndUpdate({Registration: reg}, update, { new: true });
+    // The find part requires an object rather than a string
+    const updatedCar = await Car.findOneAndUpdate(
+      { Registration: reg },
+      update,
+      { new: true }
+    );
 
-        if (updatedCar) {
-            res.send("Updated successfully");
-        } else {
-            res.status(404).send("Car not found");
-        }
-    } catch (error) {
-        console.error("Something went wrong when updating data.", error);
-        res.status(500).send("An error occurred while updating.");
+    if (updatedCar) {
+      res.send("Updated successfully");
+    } else {
+      res.status(404).send("Car not found");
     }
+  } catch (error) {
+    console.error("Something went wrong when updating data.", error);
+    res.status(500).send("An error occurred while updating.");
+  }
 };
 
 exports.deleteByReg = async (req, res) => {
-    try {
-        const reg = req.params.reg;
-        // Remove a car with the specified reg gained from the url sent ":reg"
-        const deleteResult = await Car.deleteOne({ Registration: reg });
+  try {
+    const reg = req.params.reg;
+    // Remove a car with the specified reg gained from the url sent ":reg"
+    const deleteResult = await Car.deleteOne({ Registration: reg });
 
-        if (deleteResult.deletedCount > 0) {
-            res.send("Successfully deleted the car.");
-        } else {
-            res.send("Car not found...");
-        }
-    } catch (error) {
-        console.error("An error occurred while removing car.", error);
-        res.status(500).send("An error occurred while removing car.");
+    if (deleteResult.deletedCount > 0) {
+      res.send("Successfully deleted the car.");
+    } else {
+      res.send("Car not found...");
     }
+  } catch (error) {
+    console.error("An error occurred while removing car.", error);
+    res.status(500).send("An error occurred while removing car.");
+  }
 };
-
-
