7c718795860925179a0231d33af55d76ba2a9ec3
mark <markpackham1@gmail.com>
2023-10-30 08:51:34 +0000

    add insertion script

---
+++ L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 1/scripts.js
@@ -0,0 +1,47 @@
+// Convert to single line for terminal
+// https://lingojam.com/TexttoOneLine
+
+db.cars.insertMany([
+  {
+    Model: 2005,
+    Make: "Ford Fiesta",
+    Owner: "Sue Bailey",
+    Registration: "ABC 123 GP",
+    Address: "13 Main Road, Johannesburg, South Africa",
+  },
+  {
+    Model: 2001,
+    Make: "Very Slow Car",
+    Owner: "Jim Smith",
+    Registration: "123",
+    Address: "123 Street, London, England",
+  },
+  {
+    Model: 2002,
+    Make: "Slow Car",
+    Owner: "Jane Smith",
+    Registration: "345",
+    Address: "345 Street, London, England",
+  },
+  {
+    Model: 2003,
+    Make: "Fast Car",
+    Owner: "Paul Smith",
+    Registration: "678",
+    Address: "678 Street, London, England",
+  },
+  {
+    Model: 2004,
+    Make: "Sports Car",
+    Owner: "Dave Sporty Carman",
+    Registration: "910",
+    Address: "910 Street, London, England",
+  },
+  {
+    Model: 2005,
+    Make: "Family Car",
+    Owner: "Greg McFamily",
+    Registration: "999",
+    Address: "999 Street, London, England",
+  },
+]);
