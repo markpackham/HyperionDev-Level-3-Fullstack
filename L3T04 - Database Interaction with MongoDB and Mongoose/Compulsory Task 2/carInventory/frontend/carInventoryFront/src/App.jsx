06dca5434ad2b220767a7ef962ed76082cc942c5
mark <markpackham1@gmail.com>
2023-10-30 20:13:44 +0000

    add react frontend

--- L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/frontend/carInventoryFront/src/App.jsx
+++ L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/frontend/carInventoryFront/src/App.jsx
@@ -1,13 +1,27 @@
+import { useState, useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 function App() {
+  const [cars, setCars] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/cars")
+      .then((response) => response.json())
+      .then((data) => setCars(data));
+  }, []);
+
   return (
-    <>
-      <div className="className">
-        <h1>Car Inventory</h1>
-      </div>
-    </>
+    <div className="container">
+      <h1>Car Inventory</h1>
+      <ul>
+        {cars.map((car) => (
+          <li key={car._id}>
+            {car.Model} {car.Make} {car.Owner}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
