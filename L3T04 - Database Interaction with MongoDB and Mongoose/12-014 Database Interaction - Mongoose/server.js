b0a90a14fb50710aad31449ab27052361fb86372
mark <markpackham1@gmail.com>
2023-10-30 08:00:20 +0000

    add dotenv and .env then connect to database

--- L3T04 - Database Interaction with MongoDB and Mongoose/12-014 Database Interaction - Mongoose/server.js
+++ L3T04 - Database Interaction with MongoDB and Mongoose/12-014 Database Interaction - Mongoose/server.js
@@ -1,10 +1,15 @@
+// Connect to database using password in .env via dotenv module hidden by gitignore
+const dotenv = require("dotenv");
+dotenv.config();
+const password = process.env.MONGODB_PASSWORD;
+
 // Import dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 // Import routes
-const getBlogs = require('./routes/getBlogs');
+const getBlogs = require("./routes/getBlogs");
 
 // Initialize express
 const app = express();
@@ -13,13 +18,17 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 // ! [IMPORTANT]: Replace with your mongoDB URI string. You can get it from your Atlas cluster.
-const uri = "mongodb://hyperionDB:password@hyperion-shard-00-00-f78fc.m..."
+const uri = `mongodb+srv://graveofmine99:${password}@hyperiondevlearning.dpl6f6p.mongodb.net/?retryWrites=true&w=majority`;
 
 // Connect to the database
 mongoose.Promise = global.Promise;
 mongoose.connect(uri, { useNewUrlParser: true }).then(
-  () => { console.log('Successfully connected to the database!') },
-  err => { console.log('Could not connect to the database...' + err) }
+  () => {
+    console.log("Successfully connected to the database!");
+  },
+  (err) => {
+    console.log("Could not connect to the database..." + err);
+  }
 );
 
 // Allow app to accept json and url encoded values
@@ -27,7 +36,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Set up routes to be handled from: http://localhost:8080/blogs
-app.use('/blogs', getBlogs);
+app.use("/blogs", getBlogs);
 
 // Start up express server
 app.listen(PORT, () => {
