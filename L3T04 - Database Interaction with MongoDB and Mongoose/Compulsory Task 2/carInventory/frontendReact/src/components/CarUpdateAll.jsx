d8d696fe10890d6c01687fd2a3e4273054348f0d
mark <markpackham1@gmail.com>
2023-10-31 18:32:37 +0000

    add formik for mass update

--- L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/frontendReact/src/components/CarUpdateAll.jsx
+++ L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/frontendReact/src/components/CarUpdateAll.jsx
@@ -1,6 +1,28 @@
 import PropTypes from "prop-types";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const CarUpdateAll = ({ handleUpdateAllOwner }) => {
+  // Use Formik and Yup to tell user off for not filling in fields
+  const validationSchema = Yup.object({
+    carModelUpdateAll: Yup.number().required("Model/Year is required"),
+    carMakeUpdateAll: Yup.string().required("Make is required"),
+    carRegistrationUpdateAll: Yup.string().required("Registration is required"),
+    carOwnerUpdateAll: Yup.string().required("Owner is required"),
+    carAddressUpdateAll: Yup.string().required("Address is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      carModelUpdateAll: 0,
+      carMakeUpdateAll: "",
+      carRegistrationUpdateAll: "",
+      carOwnerUpdateAll: "",
+      carAddressUpdateAll: "",
+    },
+    validationSchema,
+  });
+
   return (
     <>
       <h4>Mass Update on Owner</h4>
@@ -16,35 +38,82 @@ const CarUpdateAll = ({ handleUpdateAllOwner }) => {
           id="carModelUpdateAll"
           type="number"
           className="form-control"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.carModelUpdateAll}
         />
+        {formik.touched.carModelUpdateAll && formik.errors.carModelUpdateAll ? (
+          <div className="fw-bold text-danger mb-1">
+            {formik.errors.carModelUpdateAll}
+          </div>
+        ) : null}
+
         <label htmlFor="carMakeUpdateAll">Make:</label>
         <input
           required
           id="carMakeUpdateAll"
           type="text"
           className="form-control"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.carMakeUpdateAll}
         />
+        {formik.touched.carMakeUpdateAll && formik.errors.carMakeUpdateAll ? (
+          <div className="fw-bold text-danger mb-1">
+            {formik.errors.carMakeUpdateAll}
+          </div>
+        ) : null}
+
         <label htmlFor="carRegistrationUpdateAll">Registration:</label>
         <input
           required
           id="carRegistrationUpdateAll"
           type="text"
           className="form-control"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.carRegistrationUpdateAll}
         />
+        {formik.touched.carRegistrationUpdateAll &&
+        formik.errors.carRegistrationUpdateAll ? (
+          <div className="fw-bold text-danger mb-1">
+            {formik.errors.carRegistrationUpdateAll}
+          </div>
+        ) : null}
+
         <label htmlFor="carOwnerUpdateAll">Owner:</label>
         <input
           required
           id="carOwnerUpdateAll"
           type="text"
           className="form-control"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.carOwnerUpdateAll}
         />
+        {formik.touched.carOwnerUpdateAll && formik.errors.carOwnerUpdateAll ? (
+          <div className="fw-bold text-danger mb-1">
+            {formik.errors.carOwnerUpdateAll}
+          </div>
+        ) : null}
+
         <label htmlFor="carAddressUpdateAll">Address:</label>
         <input
           required
           id="carAddressUpdateAll"
           type="text"
           className="form-control"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.carAddressUpdateAll}
         />
+        {formik.touched.carAddressUpdateAll &&
+        formik.errors.carAddressUpdateAll ? (
+          <div className="fw-bold text-danger mb-1">
+            {formik.errors.carAddressUpdateAll}
+          </div>
+        ) : null}
+
         <button onClick={handleUpdateAllOwner} className="btn btn-warning">
           Update All on Owner
         </button>
