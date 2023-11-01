df191a1ec9c47ad925913a3277d0ab0814bdb2ed
mark <markpackham1@gmail.com>
2023-11-01 09:07:52 +0000

    add proxy retrieval code

--- L3T05 - React - Backend Integration/Tasks/frontend/src/App.js
+++ L3T05 - React - Backend Integration/Tasks/frontend/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 function App() {
+  const [data, setData] = useState({}); // State to store fetched data
+  useEffect(() => {
+    fetchData(); // Fetch data each time the component loads
+  }, []);
+  // Function to fetch data from the server
+  const fetchData = async () => {
+    try {
+      /* Sends a GET request to
+  'http://localhost:5000//api/data' (backend server) */
+      const response = await axios.get("/api/data");
+      setData(response.data); // Update state with fetched data
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Display the message, or 'Loading...' if data is not yet fetched*/}
+        <h1>{data.message || "Loading..."}</h1>
       </header>
     </div>
   );
 }
-
 export default App;
