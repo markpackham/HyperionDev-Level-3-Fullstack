e79acbdd6f23770e07a3764f205a53ee5ec8db73
mark <markpackham1@gmail.com>
2023-11-01 08:09:50 +0000

    bootstrap more responsive form

--- L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/frontendReact/src/components/CarItem.jsx
+++ L3T04 - Database Interaction with MongoDB and Mongoose/Compulsory Task 2/carInventory/frontendReact/src/components/CarItem.jsx
@@ -7,56 +7,79 @@ const CarItem = ({ cars, updateCar, deleteCar }) => {
         Model Year - Make - Registration - Owner - Address
       </h5>
       <ul>
-        {(cars.length > 0 &&
-          // Ideally the car._id that MongoDB creates on its own would be enough for the key
-          // however the state would take too long to retrieve it so I have to make do with the
-          // car.Registration which should be unique, adding the car._id to is is a safe guard
-          // against user data entry
-          cars.map((car) => (
-            <li key={car.Registration + car._id}>
-              <input
-                id={`carModel-${car.Registration}`}
-                size="6"
-                type="number"
-                defaultValue={car.Model}
-              />
-              <input
-                id={`carMake-${car.Registration}`}
-                size="10"
-                type="text"
-                defaultValue={car.Make}
-              />
-              <input
-                id={`carRegistration-${car.Registration}`}
-                size="10"
-                type="text"
-                defaultValue={car.Registration}
-              />
-              <input
-                id={`carOwner-${car.Registration}`}
-                type="text"
-                defaultValue={car.Owner}
-              />
-              <input
-                id={`carAddress-${car.Registration}`}
-                size="35"
-                type="text"
-                defaultValue={car.Address}
-              />
-              <button
-                onClick={() => updateCar(car.Registration)}
-                className="btn btn-info"
+        <div className="list-group">
+          {(cars.length > 0 &&
+            // Ideally the car._id that MongoDB creates on its own would be enough for the key
+            // however the state would take too long to retrieve it so I have to make do with the
+            // car.Registration which should be unique, adding the car._id to is is a safe guard
+            // against user data entry
+            cars.map((car) => (
+              <a
+                href="#"
+                className="list-group-item list-group-item-action"
+                key={car.Registration + car._id}
               >
-                Update
-              </button>
-              <button
-                onClick={() => deleteCar(car.Registration)}
-                className="btn btn-danger"
-              >
-                Delete
-              </button>
-            </li>
-          ))) || <h3>Loading ...</h3>}
+                <div className="row">
+                  <div className="col-sm-6 col-md-3">
+                    <input
+                      id={`carModel-${car.Registration}`}
+                      className="form-control"
+                      type="number"
+                      defaultValue={car.Model}
+                    />
+                  </div>
+                  <div className="col-sm-6 col-md-3">
+                    <input
+                      id={`carMake-${car.Registration}`}
+                      className="form-control"
+                      type="text"
+                      defaultValue={car.Make}
+                    />
+                  </div>
+                  <div className="col-sm-6 col-md-3">
+                    <input
+                      id={`carRegistration-${car.Registration}`}
+                      className="form-control"
+                      type="text"
+                      defaultValue={car.Registration}
+                    />
+                  </div>
+                  <div className="col-sm-6 col-md-3">
+                    <input
+                      id={`carOwner-${car.Registration}`}
+                      className="form-control"
+                      type="text"
+                      defaultValue={car.Owner}
+                    />
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-sm-12 col-md-9">
+                    <input
+                      id={`carAddress-${car.Registration}`}
+                      className="form-control"
+                      type="text"
+                      defaultValue={car.Address}
+                    />
+                  </div>
+                  <div className="col-sm-12 col-md-3">
+                    <button
+                      onClick={() => updateCar(car.Registration)}
+                      className="btn btn-info"
+                    >
+                      Update
+                    </button>
+                    <button
+                      onClick={() => deleteCar(car.Registration)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              </a>
+            ))) || <h3>Loading ...</h3>}
+        </div>
       </ul>
     </>
   );
