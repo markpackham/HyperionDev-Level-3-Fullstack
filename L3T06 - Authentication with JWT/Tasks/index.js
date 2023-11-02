62a19b4fec06316bfc63c1637cb3e35db2d69db8
mark <markpackham1@gmail.com>
2023-11-02 07:39:51 +0000

    give back a response

--- L3T06 - Authentication with JWT/Tasks/index.js
+++ L3T06 - Authentication with JWT/Tasks/index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8000;
+
 // Allows us to parse the body of a request
 app.use(bodyParser.json());
+
 // User login
 // http://localhost:8000/login
 app.post("/login", (req, res) => {
   // Req.body is sent by the client
   const usr = req.body.username;
   const pwd = req.body.password;
+  res.send(`Username: ${usr}, Password: ${pwd}`);
 });
+
 // Start the server
 app.listen(PORT, () =>
   console.log(`Now listening at http://localhost:${PORT}`)
