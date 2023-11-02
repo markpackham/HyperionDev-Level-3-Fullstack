2c42b945a5d7701d3b906774493eaae1226210ba
mark <markpackham1@gmail.com>
2023-11-02 07:28:21 +0000

    create express server

---
+++ L3T06 - Authentication with JWT/Tasks/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 8000;
+// Allows us to parse the body of a request
+app.use(bodyParser.json());
+// User login
+app.post("/login", (req, res) => {
+  // Req.body is sent by the client
+  const usr = req.body.username;
+  const pwd = req.body.password;
+});
+// Start the server
+app.listen(PORT, () =>
+  console.log(`Now listening at http://localhost:${PORT}`)
+);
