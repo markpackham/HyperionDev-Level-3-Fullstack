cbd2f4562c53bdad21b5b2855293ba2f70155b0d
mark <markpackham1@gmail.com>
2023-11-03 10:17:10 +0000

    add todos to database

--- L3T07 - Express - Middleware/06-014 Express - Middleware/controllers/userDB.js
+++ L3T07 - Express - Middleware/06-014 Express - Middleware/controllers/userDB.js
@@ -1,7 +1,17 @@
 // controllers/userDB.js
 const userInformation = [
-  { id: 1, username: "user1", password: "password1" },
-  { id: 2, username: "user2", password: "password2" },
+  {
+    id: 1,
+    username: "user1",
+    password: "password1",
+    todos: ["eat", "sleep", "code"],
+  },
+  {
+    id: 2,
+    username: "user2",
+    password: "password2",
+    todos: ["By groceries", "Food the dog", "Make dinner"],
+  },
   // ... other user data
 ];
 // Export the userInformation array to be used in userController.js
