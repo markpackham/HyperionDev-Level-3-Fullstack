36d45f0801a51104694772e5e9a8ad55f8589460
mark <markpackham1@gmail.com>
2023-11-03 07:41:20 +0000

    add userController

---
+++ L3T07 - Express - Middleware/06-014 Express - Middleware/controllers/userDB.js
@@ -0,0 +1,8 @@
+// controllers/userDB.js
+const userInformation = [
+  { id: 1, username: "user1", password: "password1" },
+  { id: 2, username: "user2", password: "password2" },
+  // ... other user data
+];
+// Export the userInformation array to be used in userController.js
+module.exports = userInformation;
