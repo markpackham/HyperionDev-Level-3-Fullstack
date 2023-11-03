7b47721bcbe6788774a5e20d0a7c3894bdeaf1dd
mark <markpackham1@gmail.com>
2023-11-03 10:10:26 +0000

    add screenshots from Postman

--- L3T07 - Express - Middleware/06-014 Express - Middleware/routes/loginRoute.js
+++ L3T07 - Express - Middleware/06-014 Express - Middleware/routes/loginRoute.js
@@ -2,6 +2,7 @@
 // get the userController
 const { userController } = require("../controllers/userController");
 const loginRoute = (app) => {
+  // http://localhost:8080/login?username=user1&password=password1
   app.get("/login", userController);
   //This route URL will be http://localhost:8080/login
 };
