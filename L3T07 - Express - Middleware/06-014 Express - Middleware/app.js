dabdbf51aad360965f354b8242230a4c9d2d2464
mark <markpackham1@gmail.com>
2023-11-03 12:37:17 +0000

    implement fresh code start

--- L3T07 - Express - Middleware/06-014 Express - Middleware/app.js
+++ L3T07 - Express - Middleware/06-014 Express - Middleware/app.js
@@ -1,28 +1,8 @@
 const express = require("express");
 const app = express();
 
-// Handle json via Express middleware and not body-parser
-app.use(express.json());
-
-// Global error handler middleware
-// ensures that server doesn't crash on unhandled exceptions
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Internal Server Error");
-});
-
-// import the login route
-const loginRoute = require("./routes/loginRoute.js");
-
-// import the myLoggerRoute
 const myLoggerRoute = require("./routes/myLoggerRoute");
 
-// import the secure userData route
-const userDataRoute = require("./routes/secure/userDataRoute");
-userDataRoute(app);
-
-// Step 1: call the myLoggerRoute function and pass in the app object as an argument
-loginRoute(app);
 myLoggerRoute(app);
 
 const PORT = process.env.PORT || 8080;
