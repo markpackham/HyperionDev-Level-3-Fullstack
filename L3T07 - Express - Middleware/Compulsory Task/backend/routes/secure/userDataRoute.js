88ae07a1496b75d84cccb35f46ee7d89b874d630
mark <markpackham1@gmail.com>
2023-11-06 09:06:06 +0000

    reduce unwanted doc

--- L3T07 - Express - Middleware/Compulsory Task/backend/routes/secure/userDataRoute.js
+++ L3T07 - Express - Middleware/Compulsory Task/backend/routes/secure/userDataRoute.js
@@ -1,10 +1,6 @@
-// routes/loginRoute.js
-// get the userController
 const { getTodos } = require("../../controllers/userController");
 const { jwtMiddleware } = require("../../middleware/jwtMiddleware");
 const userDataRoute = (app) => {
   app.post("/login/data", jwtMiddleware, getTodos);
-  //This route URL will be http://localhost:8080/login
 };
-// export the login function to be used in "../app.js"
 module.exports = userDataRoute;
