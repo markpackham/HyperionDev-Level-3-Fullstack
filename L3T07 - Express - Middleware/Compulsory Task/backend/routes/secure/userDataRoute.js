dcdbc668017e176592ee781e818c705d256b7305
mark <markpackham1@gmail.com>
2023-11-06 10:17:40 +0000

    use get for fetching todos

--- L3T07 - Express - Middleware/Compulsory Task/backend/routes/secure/userDataRoute.js
+++ L3T07 - Express - Middleware/Compulsory Task/backend/routes/secure/userDataRoute.js
@@ -1,6 +1,6 @@
 const { getTodos } = require("../../controllers/userController");
 const { jwtMiddleware } = require("../../middleware/jwtMiddleware");
 const userDataRoute = (app) => {
-  app.post("/login/data", jwtMiddleware, getTodos);
+  app.get("/login/data", jwtMiddleware, getTodos);
 };
 module.exports = userDataRoute;
