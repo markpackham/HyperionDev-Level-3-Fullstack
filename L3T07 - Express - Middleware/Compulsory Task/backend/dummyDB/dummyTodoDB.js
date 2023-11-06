7dfe9984fc4348e5aaa67484c8f30a714e0adab6
mark <markpackham1@gmail.com>
2023-11-06 09:41:51 +0000

    correct todo labels

--- L3T07 - Express - Middleware/Compulsory Task/backend/dummyDB/dummyTodoDB.js
+++ L3T07 - Express - Middleware/Compulsory Task/backend/dummyDB/dummyTodoDB.js
@@ -1,13 +1,13 @@
 const todosDB = [
   {
-    task_id: 1,
-    task_name: "mow lawn",
-    task_description: "mow lawn in garden",
+    todo_id: 1,
+    todo_name: "mow lawn",
+    todo_description: "mow lawn in garden",
   },
   {
-    task_id: 2,
-    task_name: "shopping",
-    task_description: "buy bread, ham and cheese",
+    todo_id: 2,
+    todo_name: "shopping",
+    todo_description: "buy bread, ham and cheese",
   },
 ];
 
