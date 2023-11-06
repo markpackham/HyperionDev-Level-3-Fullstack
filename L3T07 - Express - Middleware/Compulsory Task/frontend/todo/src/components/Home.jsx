03f192fa2d46f6bed3c1916dc99a5eb4e8faa818
mark <markpackham1@gmail.com>
2023-11-06 09:47:39 +0000

    add todo frontend structure

--- L3T07 - Express - Middleware/Compulsory Task/frontend/todo/src/components/Home.jsx
+++ L3T07 - Express - Middleware/Compulsory Task/frontend/todo/src/components/Home.jsx
@@ -1,5 +1,93 @@
+import { useState } from "react";
+
 const Home = () => {
-  return <div>Home</div>;
+  const [todos, setTodos] = useState([
+    {
+      todo_id: 1,
+      todo_name: "mow lawn",
+      todo_description: "mow lawn in garden",
+    },
+  ]);
+
+  const [todo, setTodo] = useState({
+    todo_id: "",
+    todo_name: "",
+    todo_description: "",
+  });
+
+  // Add todo
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  };
+
+  // Delete todo
+  const deleteTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.todo_id !== id));
+  };
+
+  // Update todo
+  const updateTodo = (id, updatedTodo) => {
+    setTodos(todos.map((todo) => (todo.todo_id === id ? updatedTodo : todo)));
+  };
+
+  // Update state as I type in form
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setTodo({
+      ...todo,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div>
+      <form onSubmit={addTodo}>
+        <label>
+          Todo ID:
+          <input
+            type="number"
+            name="todo_id"
+            value={todo.todo_id}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Todo Name:
+          <input
+            type="text"
+            name="todo_name"
+            value={todo.todo_name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Todo Description:
+          <input
+            type="text"
+            name="todo_description"
+            value={todo.todo_description}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.todo_id}>
+            {todo.todo_name} - {todo.todo_description}
+            <button onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
+            <button onClick={() => updateTodo(todo.todo_id, todo)}>
+              Update
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Home;
