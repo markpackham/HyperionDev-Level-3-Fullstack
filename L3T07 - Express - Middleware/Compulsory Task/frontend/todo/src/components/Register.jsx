fc2bfc1867ec93c38ee2cea90e8dcbf0cfcbb020
mark <markpackham1@gmail.com>
2023-11-06 16:39:00 +0000

    create register form outline

--- L3T07 - Express - Middleware/Compulsory Task/frontend/todo/src/components/Register.jsx
+++ L3T07 - Express - Middleware/Compulsory Task/frontend/todo/src/components/Register.jsx
@@ -2,8 +2,46 @@
 //React JS form validation: Axios User Registration Form SUBMIT: Beginners to intermediate (2021) YouTube.
 //Available at: https://youtu.be/brcHK3P6ChQ?feature=shared&amp;t=120 (Accessed: 06 November 2023).
 
+import { useState } from "react";
+import axios from "axios";
+
 const Register = () => {
-  return <div>Register</div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/register", {
+      username,
+      password,
+    });
+    console.log(response.data);
+  };
+
+  return (
+    <div>
+      {" "}
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username Email:
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
