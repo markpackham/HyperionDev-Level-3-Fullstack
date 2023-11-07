8710f6af9db311e523111e0f7c6687dbb086a95e
mark <markpackham1@gmail.com>
2023-11-07 18:11:31 +0000

    use formik for register form

--- L3T07 - Express - Middleware/Compulsory Task/frontend/todo/src/components/users/Register.jsx
+++ L3T07 - Express - Middleware/Compulsory Task/frontend/todo/src/components/users/Register.jsx
@@ -1,14 +1,42 @@
-// Learning Register form from
-// React JS form validation | axios user registration form submit | beginners to intermediate (2021) YouTube.
-// Available at: https://www.youtube.com/watch?v=brcHK3P6ChQ&amp;t=123s (Accessed: 07 November 2023).
-
-import { useRef, useState, useEffect } from "react";
+import { Formik, Form, Field } from "formik";
+import axios from "axios";
+import DOMPurify from "dompurify";
 
 const Register = () => {
+  const handleSubmit = (values) => {
+    const requestOptions = {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username: DOMPurify.sanitize(values.username),
+        password: DOMPurify.sanitize(values.password),
+      }),
+    };
+    axios
+      .post("/todos/register", requestOptions)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <div>
-      <h3>Register</h3>
-    </div>
+    <Formik
+      initialValues={{ username: "", password: "" }}
+      onSubmit={handleSubmit}
+    >
+      <Form>
+        <label>
+          Username:
+          <Field type="text" name="username" />
+        </label>
+        <label>
+          Password:
+          <Field type="password" name="password" />
+        </label>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </Form>
+    </Formik>
   );
 };
 
