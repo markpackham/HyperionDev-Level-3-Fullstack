f6453eb52d2ec8aad9c18b511363065ad5efd5f8
mark <markpackham1@gmail.com>
2023-11-08 14:09:00 +0000

    add json middleware check

--- L3T07 - Express - Middleware/Compulsory Task/backend/routes/secure/getTodos.js
+++ L3T07 - Express - Middleware/Compulsory Task/backend/routes/secure/getTodos.js
@@ -2,9 +2,10 @@ const express = require("express");
 // Express Router used to interact with CRUD functions
 const router = express.Router();
 const todoController = require("../../controllers/todos.controller");
+const { jsonCheckMiddleware } = require("../../middleware/jsonCheckMiddleware");
 const {
   todoTooLargeMiddleware,
-} = require("../../middleware/todoTooLarge.Middleware");
+} = require("../../middleware/todoTooLargeMiddleware");
 
 // GET (default path)
 // http://localhost:8080/todos
@@ -12,7 +13,11 @@ router.get("/secure/", todoController.findAll);
 
 // POST
 // /add
-router.post("/secure/add", todoTooLargeMiddleware, todoController.create);
+router.post(
+  "/secure/add",
+  [todoTooLargeMiddleware, jsonCheckMiddleware],
+  todoController.create
+);
 
 // PUT
 // /update-todo/123
